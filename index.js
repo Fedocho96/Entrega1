9af933fe034dc3446f4fceb64c73524864b8eb6f
Federico Rosales <fedocho.96@gmail.com>
2024-11-28 21:35:42 -0300

    codigo comentado y explicado

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 
+//selecciono todos los elementos con la clase slides img y un contador para el indice en 0
 const slides = document.querySelectorAll(".slides img");
 let slideIndex = 0;
 let intervalId = null;
 
+//iniciamos el slider en cuanto cargue el cotenido del dom
 document.addEventListener("DOMContentLoaded", initializeSlider);
 
+
+//se le asigan la clase "displayslide a la imagen con el indice activo"
 function initializeSlider(){
     if(slides.length > 0){
         slides[slideIndex].classList.add("displaySlide");
@@ -12,6 +16,8 @@ function initializeSlider(){
     }
 }
 
+
+//creamos cindicionales para los limites del contador y luego la funcion que remueve y agrega la clase displayslide
 function showSlide(index){
     if(index >= slides.length){
         slideIndex = 0;
@@ -26,6 +32,8 @@ function showSlide(index){
     slides[slideIndex].classList.add("displaySlide");
 }
 
+
+//funciones para sumar o restar el indice de las imagenes
 function prevSlide(){
     clearInterval(intervalId);
     slideIndex--;
@@ -38,14 +46,15 @@ function nextSlide(){
 }
 
 
-//document.querySelector(".containerPadre").style.backgroundImage = "url('../images/image1.jpg')"
-
 
 //dark mode boton
 
+
+//obtenemos el elemento con el Id indicado
 let darkmode = localStorage.getItem('darkmode')
 const themeSwitch = document.getElementById('theme-switch')
 
+//ambas funciones para agregar o quitar el estilo "darkmode" al elemento
 const enableDarkmode=()=>{
     document.body.classList.add('darkmode')
     localStorage.setItem('darkmode', 'active')
@@ -58,7 +67,8 @@ const disableDarkmode=()=>{
 
 if(darkmode === "active") enableDarkmode()
 
-
+//funcion que "escucha" el evento al hacer click en el boton, la cual almacena el estilo para mantenerlo activo en caso de refrescar la pagina y cambia el estilo
+// en base a un condicional
 themeSwitch.addEventListener("click", ()=>{
     darkmode = localStorage.getItem('darkmode')
     darkmode !== "active" ? enableDarkmode() : disableDarkmode()
